9b2d45a8a80050e194a073eff38013834f2e1157
SebastianSassone <sassonejuansebastian@gmail.com>
2023-01-03 16:04:59 -0300

    commit css

--- js/proyectos.js
+++ js/proyectos.js
@@ -13,46 +13,44 @@ window.addEventListener('load', () =>{
 
 //Mostrar mas
 
-let button_mas_1 = document.getElementById('button_mas_1')
-let button_mas_2 = document.getElementById('button_mas_2')
-let button_mas_3 = document.getElementById('button_mas_3')
-let button_mas_4 = document.getElementById('button_mas_4')
+// let button_mas_1 = document.getElementById('button_mas_1')
+// let button_mas_2 = document.getElementById('button_mas_2')
+// let button_mas_3 = document.getElementById('button_mas_3')
+// let button_mas_4 = document.getElementById('button_mas_4')
 
+// let boton_cerr = document.getElementById('boton_cerrar_id');
+// let mos_mas = document.getElementById('mos_mas');
+// let section_proyectos = document.getElementById('section_proyectos');
 
-let boton_cerr = document.getElementById('boton_cerrar_id');
-let mos_mas = document.getElementById('mos_mas');
-let section_proyectos = document.getElementById('section_proyectos');
+// let h2_c = section_proyectos.querySelectorAll('h2')   
+// let p_c  = section_proyectos.querySelectorAll('p')  
 
-let h2_c = section_proyectos.querySelectorAll('h2')   
-let p_c  = section_proyectos.querySelectorAll('p')  
+// button_mas_1.addEventListener('click', () => {
+//   mos_mas.classList.add('mos_mos_mas')
+//   let h2 = h2_c[1].textContent 
+//   h2_c[0].innerHTML = h2
+//   let p = p_c[1].textContent
+//   p_c[0].innerHTML = p
+// })
 
-button_mas_1.addEventListener('click', () => {
-  mos_mas.classList.add('mos_mos_mas')
-  let h2 = h2_c[1].textContent 
-  h2_c[0].innerHTML = h2
-  let p = p_c[1].textContent
-  p_c[0].innerHTML = p
-})
+// button_mas_2.addEventListener('click', () => {
+//   mos_mas.classList.add('mos_mos_mas')
+//   let h2 = h2_c[2].textContent 
+//   h2_c[0].innerHTML = h2
+//   let p = p_c[2].textContent
+//   p_c[0].innerHTML = p
+// })
 
-button_mas_2.addEventListener('click', () => {
-  mos_mas.classList.add('mos_mos_mas')
-  let h2 = h2_c[2].textContent 
-  h2_c[0].innerHTML = h2
-  let p = p_c[2].textContent
-  p_c[0].innerHTML = p
-})
+// button_mas_3.addEventListener('click', () => {
+//   mos_mas.classList.add('mos_mos_mas')
+//   let h2 = h2_c[3].textContent 
+//   h2_c[0].innerHTML = h2
+//   let p = p_c[3].textContent
+//   p_c[0].innerHTML = p
+// })
 
-button_mas_3.addEventListener('click', () => {
-  mos_mas.classList.add('mos_mos_mas')
-  let h2 = h2_c[3].textContent 
-  h2_c[0].innerHTML = h2
-  let p = p_c[3].textContent
-  p_c[0].innerHTML = p
-})
-
-
-boton_cerr.addEventListener('click', () => {
-  mos_mas.classList.remove('mos_mos_mas')
-})
+// boton_cerr.addEventListener('click', () => {
+//   mos_mas.classList.remove('mos_mos_mas')
+// })
 
 
