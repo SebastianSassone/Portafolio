90825fdcc16142f111ded62b95907e70b24a2735
SebastianSassone <sassonejuansebastian@gmail.com>
2023-02-17 12:20:51 -0300

    add new project

--- js/proyectos.js
+++ js/proyectos.js
@@ -4,12 +4,14 @@ let div_proyecto_1 = document.getElementById('div_proyecto_1')
 let div_proyecto_2 = document.getElementById('div_proyecto_2')
 let div_proyecto_3 = document.getElementById('div_proyecto_3')
 let div_proyecto_4 = document.getElementById('div_proyecto_4')
+let div_proyecto_5 = document.getElementById('div_proyecto_5')
 
 window.addEventListener('load', () =>{ 
     div_proyecto_1.classList.add('div_proyecto')
     div_proyecto_2.classList.add('div_proyecto')
     div_proyecto_3.classList.add('div_proyecto')
-    div_proyecto_4.classList.add('div_proyecto')})
+    div_proyecto_4.classList.add('div_proyecto')
+    div_proyecto_5.classList.add('div_proyecto')})
 
 //Mostrar mas
 
