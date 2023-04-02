72f2a69f5299e14690fc4a910771139eef7c4f80
SebastianSassone <sassonejuansebastian@gmail.com>
2023-04-02 00:20:41 -0300

    routes changes

--- js/proyectos.js
+++ js/proyectos.js
@@ -5,13 +5,15 @@ let div_proyecto_2 = document.getElementById('div_proyecto_2')
 let div_proyecto_3 = document.getElementById('div_proyecto_3')
 let div_proyecto_4 = document.getElementById('div_proyecto_4')
 let div_proyecto_5 = document.getElementById('div_proyecto_5')
+let div_proyecto_6 = document.getElementById('div_proyecto_6')
 
 window.addEventListener('load', () =>{ 
     div_proyecto_1.classList.add('div_proyecto')
     div_proyecto_2.classList.add('div_proyecto')
     div_proyecto_3.classList.add('div_proyecto')
     div_proyecto_4.classList.add('div_proyecto')
-    div_proyecto_5.classList.add('div_proyecto')})
+    div_proyecto_5.classList.add('div_proyecto')
+    div_proyecto_6.classList.add('div_proyecto')})
 
 //Mostrar mas
 
